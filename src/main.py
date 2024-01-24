738a3fd5632d867d7dda255057b4fefeeeb2f3d6
[200~and the repository exists <sassassas107@gmail.com>
2024-01-24 19:42:43 +0600

    add redis caches and asycn.await

--- src/main.py
+++ src/main.py
@@ -1,8 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from aioredis import create_redis_pool, Redis
 from product.routers.products import product_router
 from database import SQLALCHEMY_DATABASE_URL
 import asyncpg
-
 
 app = FastAPI(
     title="Amazon"
@@ -11,9 +11,15 @@ app = FastAPI(
 @app.on_event("startup")
 async def startup_db_client():
     app.state.pool = await asyncpg.create_pool(SQLALCHEMY_DATABASE_URL)
+    app.state.redis = await create_redis_pool('redis://localhost')
 
 @app.on_event("shutdown")
 async def shutdown_db_client():
     await app.state.pool.close()
+    app.state.redis.close()
+    await app.state.redis.wait_closed()
 
 app.include_router(product_router)
+
+async def get_redis(app: FastAPI = Depends()):
+    return app.state.redis
--- src/product/crud/products.py
+++ src/product/crud/products.py
@@ -1,6 +1,12 @@
 from sqlalchemy.orm import Session
 from ..models import products as models
 from ..schemas.products import ProductCreate, ProductRead
+from aioredis import Redis
+from main import FastAPI , Depends
+
+
+async def get_redis(app: FastAPI = Depends()):
+    return app.state.redis
 
 
 def crud_create_product(db: Session, product: ProductCreate) -> ProductRead:
@@ -18,8 +24,12 @@ def crud_create_product(db: Session, product: ProductCreate) -> ProductRead:
 
     return ProductRead(id=product_id, **product.dict())
 
-def crud_get_product(db: Session, product_id: int):
-    return db.query(models.product_table).filter(models.product_table.c.id == product_id).first()
+async def crud_get_product(db: Session, product_id: int, redis: Redis = Depends(get_redis)):
+    product = await redis.get(f'product:{product_id}')
+    if product is None:
+        product = db.query(models.product_table).filter(models.product_table.c.id == product_id).first()
+        await redis.set(f'product:{product_id}', product)
+    return product
 
 def crud_get_all_products(db: Session, skip: int = 0, limit: int = 10):
     return db.query(models.product_table).offset(skip).limit(limit).all()
